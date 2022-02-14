9ca7bdc8c699376840365cbd9ade6f12c6a7e25c
Mateus Ribeiro <mateus.vaz@ufu.br>
2022-02-14 11:49:57 -0300

    Atualização da função menu

--- main.cpp
+++ main.cpp
@@ -1,5 +1,9 @@
+// Bibliotecas padrão
 #include <stdio.h>
 #include <stdlib.h>
+
+// UTF-8
+#include <locale.h>
 
 struct a_definir{
 	//inserir código aqui
@@ -20,13 +24,18 @@ void mostra1(struct a_definir T[]){
 void menu(){
 	int opcao;
 	struct a_definir P[30];
+    setlocale(LC_ALL,"");
 
 	while(1){
-		printf("\nBem vindo ao Sistema de... ");
-		printf("\n1- Cadastrar ");
-		printf("\n2- Mostrar Todos");
-		printf("\n3- Mostrar um");
-		printf("\n9- Sair ");
+		printf("\nBem vindo ao Sistema de Gerenciamento de Restaurante");
+		printf("\n1 - Mostrar o cardapio");
+		printf("\n2 - Mostrar o estoque");
+		printf("\n3 - Cadastrar pedido");
+		printf("\n4 - Mostrar pedidos");
+		printf("\n5 - Controle de caixa");
+		printf("\n6 - Relatorio gerencial");
+		printf("\n7 - Ver relatorio");
+		printf("\n9 - Sair");
 		printf("\nDigite opção: ");
 		scanf("%d", &opcao);
 	
