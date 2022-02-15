2e4ea6abb524a57cfbeb2807ea93ced744160914
Mateus Ribeiro <mateus.vaz@ufu.br>
2022-02-15 18:10:52 -0300

    Mostar cardapio + Adicionar cardapio

--- main.cpp
+++ main.cpp
@@ -2,38 +2,93 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// UTF-8
-#include <locale.h>
-
 typedef struct{
-	//a definir
-}a_definir;
+	char vet[100];
+}pratos_principais;
 
+void mostrar_cardapio(int *qtd_comida, pratos_principais T[]){
+	int op;
+	while(1){
+		printf("\n===== Mostrar cardapio =====");
+		setbuf(stdin, NULL);
+		printf("\n1. Mostrar pratos principais");
+		printf("\n2. Mostrar sobremesas");
+		printf("\n3. Mostrar bebidas");
+		printf("\n4. Voltar");
+		printf("\nDigite a opcao: ");
+		scanf("%d", &op);
 
-void cadastro(a_definir T[]){
-	//inserir código aqui
+		if(op == 1){
+			printf("\n===== PRATOS PRINCIPAIS =====");
+			for(int i = 0; i < *qtd_comida; i++){
+				printf("\n%s", T[i].vet);
+			}
+			printf("\n");
+		}
+
+		if(op == 4) break;
+	}
+}
+
+void adicionar_cardapio(int *qtd_comida, pratos_principais T[]){
+	int op, op2;
+
+	while(1){
+		printf("\n===== Adicionar no cardapio =====");
+		setbuf(stdin, NULL);
+		printf("\n1. Adicionar em pratos principais");
+		//printf("\n2. Adicionar em sobremesas");
+		//printf("\n3. Adicionar em bebidas");
+		printf("\n4. Voltar");
+		printf("\nDigite a opcao: ");
+		scanf("%d", &op);
+
+		if(op == 1){
+			while(1){
+				setbuf(stdin, NULL);
+				printf("\nDigite o nome do prato: ");
+				scanf("%[^\n]s", T[*qtd_comida].vet);
+				printf("\n1. Para adicionar outro prato");
+				printf("\n2. Para sair");
+				printf("\nDigite a opcao: ");
+				scanf("%d", &op2);
+
+				*qtd_comida += 1;
+				if(op2 == 2) break;
+			}
+		}
+
+		if(op == 4) break;
+	}
 }
 
 
 void menu(){
-	int opcao;
-	a_definir P[30];
-    setlocale(LC_ALL,"");
+	int opcao, quantidade_comida = 0;;
+	int *quantidadeComida;
+	quantidadeComida = &quantidade_comida;
+
+	pratos_principais c[50];
 
 	while(1){
+		setbuf(stdin, NULL);
 		printf("\nBem vindo ao Sistema de Gerenciamento de Restaurante");
 		printf("\n1 - Mostrar o cardapio");
-		printf("\n2 - Mostrar o estoque");
-		printf("\n3 - Cadastrar pedido");
-		printf("\n4 - Mostrar pedidos");
-		printf("\n5 - Controle de caixa");
-		printf("\n6 - Relatorio gerencial");
-		printf("\n7 - Ver relatorio");
+		printf("\n2 - Adicionar no cardapio");
+		/*printf("\n3 - Mostrar o estoque");
+		printf("\n4 - Cadastrar pedido");
+		printf("\n5 - Mostrar pedidos");
+		printf("\n6 - Controle de caixa");
+		printf("\n7 - Relatorio gerencial");
+		printf("\n8 - Ver relatorio");
 		printf("\n9 - Sair");
-		printf("\nDigite opção: ");
+		*/
+
+		printf("\nDigite opcao: ");
 		scanf("%d", &opcao);
 	
-		if(opcao == 1) cadastro(P);
+		if(opcao == 1) mostrar_cardapio(quantidadeComida, c);
+		if(opcao == 2) adicionar_cardapio(quantidadeComida, c);
 		if(opcao == 9) return;
 	}
 }
